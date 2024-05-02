8498e97c8e0c358331fda467ebc1badff5344eff
Cristina Nogales <criss@Cristinas-MacBook-Air.local>
2024-05-02 22:39:47 +0100

    part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}{text === "positive" ? "%" : null}</td>
+  </tr>
+)
+
+const Statistics = ({good, bad, neutral}) => {
+  const total = bad + good + neutral
+
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={(good - bad) / total} />
+        <StatisticLine text="positive" value={(good / total) * 100} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+
+      <br/>
+      <h1>Statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  )
+}
+
+export default App
