0f3fca4b88d1a6ebe5f881dcd83e017bbe867732
Cristina Nogales <criss@cristinas-air.home>
2024-05-26 18:30:13 +0100

    part3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -47,16 +47,22 @@ app.get('/api/persons', (request, response) => {
 // })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person =>
-        response.json(person)
-    )
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
