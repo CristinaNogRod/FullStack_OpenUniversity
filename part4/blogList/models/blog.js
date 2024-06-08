4c6d5c22cbe0e181118b6634abb1cd9bcb1681e3
CrisNog <cnogales.rod@gmail.com>
2024-06-08 14:14:58 +0100

    part4- after submit

--- part4/blogList/models/blog.js
+++ part4/blogList/models/blog.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
+const schema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
-module.exports = mongoose.model('Blog', blogSchema)
+
+schema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', schema)
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,25 +1,67 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) return null
-  
-    const favorite = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current))
-    return {
-      title: favorite.title,
-      author: favorite.author,
-      likes: favorite.likes
+  return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, {});
+}
+
+const mostBlogs = (blogs) => {
+  if (!blogs.length) {
+    return null
+  }
+
+  const authors = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1
+    return acc;
+  }, {});
+
+  let maxAuthor = Object.keys(authors)[0]
+
+  for (const author in authors) {
+    if (authors[author] > authors[maxAuthor]) {
+      maxAuthor = author
     }
   }
-  
-  
+
+  return {
+    author: maxAuthor,
+    blogs: authors[maxAuthor]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (!blogs.length) {
+    return null
+  }
+
+  const authors = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes
+    return acc;
+  }, {});
+
+  let maxAuthor = Object.keys(authors)[0]
+
+  for (const author in authors) {
+    if (authors[author] > authors[maxAuthor]) {
+      maxAuthor = author
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    likes: authors[maxAuthor]
+  }
+}
+
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
